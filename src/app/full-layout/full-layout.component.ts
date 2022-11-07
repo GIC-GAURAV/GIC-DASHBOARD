333c7d7e12c20e9d0033a40ede2a430bc1b9cedd
abhishek <abhishek.parihar@gridinfocom.com>
2022-11-07 17:26:35 +0530

    update

--- src/app/full-layout/full-layout.component.ts
+++ src/app/full-layout/full-layout.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, OnInit,ViewChild, } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, } from '@angular/core';
 import { Router } from '@angular/router';
-import { DrawerItem, DrawerSelectEvent } from '@progress/kendo-angular-layout';
+import { DrawerItem } from '@progress/kendo-angular-layout';
 
 @Component({
   selector: 'app-full-layout',
@@ -10,39 +10,134 @@ import { DrawerItem, DrawerSelectEvent } from '@progress/kendo-angular-layout';
 
 
 export class FullLayoutComponent implements OnInit {
-  @ViewChild('drawer') drawer!: ElementRef ;
-
-
-  custometoggle:boolean=false;
-  constructor( private router:Router) {
-    
+  @ViewChild('drawer') drawer!: ElementRef;
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
   }
+  subItems:any
   public items: Array<customDrawerItem> = [
-    { text: "dashboards", icon: "k-i-inbox",path:"application/to-do-list" },
-    { text: "pages", icon: "icon-social-youtube" ,path:"application/survey"},
-    { text: "Applications", icon: "k-icon k-i-calendar",path:"application/chats" },
-    { text: "UI", icon:"k-i-clip" ,path:"application/survey"},
-    { text: "Menu", icon: "k-icon k-i-calendar",path:"application/chats" },
-    { text: "Blank Page", icon: "k-icon k-i-calendar",path:"application/chats" },
-    { text: "Docs", icon: "k-i-files",path:"application/chats" },
-    // { separator: true },
-    // { text: "Attachments", icon: "k-i-envelop-link" },
-    // { text: "Favourites", icon: "k-i-star-outline" },
+    {
+      text: "dashboards", icon: "icon-menu", path: "application/to-do-list",active:true,
+
+      subItems: [{
+        text: "dashboards", icon: "icon-envelope", path: "application/to-do-list"
+      }]
+    },
+    {
+      text: "pages", icon: "icon-fire", path: "application/survey",active:false,
+      subItems: []
+    },
+    {
+      text: "Applications", icon: "k-icon k-i-calendar", path: "application/chats",active:false,
+      subItems: [{
+        text: "To-do List", icon: "icon-check", path: "application/to-do-list",
+      },
+      {
+        text: "Survey", icon: "icon-envelope", path: "application/survey",
+      },
+      {
+        text: "chats", icon: "icon-speech", path: "application/chats",
+      }
+    
+    
+    ]
+    },
+    {
+      text: "UI", icon: "k-i-clip", path: "application/survey",active:false,
+      subItems: [{
+        text: "dashboards", icon: "icon-envelope", path: "application/to-do-list",
+      }]
+    },
+    {
+      text: "Menu", icon: "k-icon k-i-calendar", path: "application/chats",active:false,
+      subItems: [{
+        text: "dashboards", icon: "icon-envelope", path: "application/to-do-list",
+      }]
+    },
+    {
+      text: "Blank Page", icon: "k-icon k-i-calendar", path: "application/chats",active:false, subItems: [{
+        text: "dashboards", icon: "k-i-inbox", path: "application/to-do-list",
+      }]
+    },
+    {
+      text: "Docs", icon: "k-i-files", path: "application/chats",active:false, subItems: [{
+        text: "dashboards", icon: "k-i-inbox", path: "application/to-do-list",
+      }]
+    },
   ];
 
-  public onSelect(ev: any): void {
-    this.custometoggle=true;
+
+
+
+  public onSelect(ev: any ,event2:any): void {
+    console.log(ev)
+    this.items.map(el=>{
+      if(ev.text === el.text){
+        el.active = true
+      }else{
+        el.active = false
+      }
+    })
+  
+    if(event2.expanded === false){
+      if(ev.subItems.length !== 0){
+        event2.toggle()
+        this.subItems=ev.subItems
+      }
+    }else{
+      if(ev.subItems.length !== 0){
+        event2.toggle()
+        this.subItems=ev.subItems
+      }
+    }
+  }
+
+  onSelectSubItem(ev: any ,event2:any){
+    if(event2.expanded === false){
+      event2.width = 180
+      event2.toggle()
+      this.subItems=ev.subItems
+     
+    }else{
+     
+    }
     // console.log(ev)
     this.router.navigate([ev.path])
-    // drawer2?.toogle()
   }
- 
+  check: string = 'open'
+  toggleUpDown(event: any, event2: any) {
+    console.log(event)
+    console.log(event2)
+    if (event.expanded === true) {
+      if (event2.expanded === true) {
+        event2.toggle()
+        this.check = 'close'
+      } else {
+        if (this.check === 'close') {
+          event.toggle()
+          this.check = 'open'
+
+        } else {
+          this.subItems=this.items[0].subItems
+          event2.width = 180
+          event2.toggle()
+        }
+      }
+    } else {
+      event.width = 100
+      event.toggle()
+    }
+  }
+
 }
 
 interface customDrawerItem extends DrawerItem {
-  path:string
+  path: string
+  subItems: object
+  active:boolean
 }
+
+
 
