9337df7a14ba6af38ca9accdf792c48873444b03
pinkee kumari <pinkee.kumari@gridinfocom.com>
2022-11-08 14:22:53 +0530

    surveyui update

--- src/app/full-layout/application/survey/survey.component.ts
+++ src/app/full-layout/application/survey/survey.component.ts
@@ -15,7 +15,29 @@ export class SurveyComponent implements OnInit {
   public listItems: Array<string> = [
     "Action",
     "Another Action",
-   
+
+  ];
+  public items: Array<any> = [
+    {
+      title: "Developer Survey",
+      subtitle: "Development",
+      date: "17.05.2021",
+      catogry: "EDUCATION"
+    },
+    {
+      title: "Designer Survey",
+      subtitle: "Workplace",
+      date: "15.05.2021",
+      catogry: "NEW FRAMEWORK"
+    },
+    {
+      title: "Backend Survey",
+      subtitle: "Hardware",
+      date: "12.05.2021",
+      catogry: "PERSONAL"
+    },
+
+
   ];
 
 }
